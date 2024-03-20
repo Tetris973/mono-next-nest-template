52a3757dea8c64772e517c8887c68a4dd9b683c3
Tetris973 <cedric.olender@gmail.com>
2024-03-20 07:05:12 +0000

    Implement the user service

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import config from './config/configuration';
 import { validate } from './config/env.validation';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,6 +17,8 @@ import { validate } from './config/env.validation';
       load: [config], // Loaded env variable after parse
       isGlobal: true, // Enable use module globally without import in each module
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
