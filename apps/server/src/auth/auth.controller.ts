7b1be1d8cb79b563577c6ec5af49c4c0b1ff50f7
Tetris973 <cedric.olender@gmail.com>
2024-03-21 04:32:33 +0000

    Add Auth guard

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -1,13 +1,32 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login') // TODO: Use a DTO with class-validator rules instead of a Record<string, any>
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  // TODO: Replace 'any' with the actual request type that conatins a user property
+  getProfile(@Request() req: any) {
+    return req.user;
   }
 }
