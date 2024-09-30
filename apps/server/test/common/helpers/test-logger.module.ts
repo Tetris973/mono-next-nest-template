87fa3e31891e74ed0caaebc6515374b5a53e9325
Tetris973 <cedric.olender@gmail.com>
2024-09-30 14:00:13 +0000

    Fix eslint rules and improve backend log testing helper

--- apps/server/test/common/helpers/test-logger.module.ts
+++ apps/server/test/common/helpers/test-logger.module.ts
@@ -1,62 +1,39 @@
-import { DestinationStream } from 'pino';
+import { TestLoggerService } from './test-logger.service';
 import { LoggerModule } from 'nestjs-pino';
-
-interface LogEntry extends Record<string, unknown> {
-  context?: string;
-}
-
-/**
- * A custom stream implementation for capturing log messages in memory.
- * This class is used to store log entries during testing.
- */
-export class TestStream implements DestinationStream {
-  messages: LogEntry[] = [];
-
-  /**
-   * Writes a log message to the in-memory store.
-   * @param msg - The log message as a JSON string.
-   */
-  write(msg: string) {
-    this.messages.push(JSON.parse(msg));
-  }
-}
-
-export const testStream = new TestStream();
+import { LogLevel } from '@server/config/log.enum';
+export { LogLevel };
+import { Module } from '@nestjs/common';
 
 /**
- * Retrieves all captured log entries.
- * @returns An array of LogEntry objects representing all captured logs.
- * @note
- * - Logs logged in the array are separate between each test files, so the logs don't collides.
- * - If you want to have logs for only a specific test case, use clearTestLogs() in beforeEach()
+ * The service that collects the logs is created outside the module and not using nestjs pattern.
+ * This current implmentation works and does not cause problem fro testing but it is not ideal.
+ * Did not manage to make a correct implemntation but for the moment it is enough.
  */
-export function getTestLogs(): LogEntry[] {
-  return testStream.messages;
-}
-
-/**
- * Clears all captured log entries.
- * This is useful for resetting the log state between tests.
- */
-export function clearTestLogs(): void {
-  testStream.messages = [];
-}
-
-export function getTestLogsByContext(context: string): LogEntry[] {
-  return testStream.messages.filter((log) => log.context === context);
-}
+const testLoggerService = new TestLoggerService();
 
 /**
  * A pre-configured NestJS Logger module for testing purposes.
  * This module uses the TestStream to capture logs in memory,
  * allowing for easy assertion and inspection in tests.
  */
-export const TestLoggerModule = LoggerModule.forRoot({
-  pinoHttp: {
-    stream: testStream,
-    level: 'trace',
-    customProps: () => ({
-      context: 'HTTP',
+@Module({
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        stream: { write: (msg) => testLoggerService.write(msg) },
+        level: 'trace',
+        customProps: () => ({
+          context: 'HTTP',
+        }),
+      },
     }),
-  },
-});
+  ],
+  /**
+   * It is important to useValue here of the object created outide the module, otherwise nestjs created its own instance
+   * that is not shared with the one used in the stream above.
+   * This results in no logs being captured by the testLoggerService.
+   */
+  providers: [{ provide: TestLoggerService, useValue: testLoggerService }],
+  exports: [TestLoggerService],
+})
+export class TestLoggerModule {}
